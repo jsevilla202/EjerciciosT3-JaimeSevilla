d89ca43eee106ad6a6456497c002c81e1ae86e83
jsevilla <jsevilla@106-19.info2>
2022-11-09 14:39:48 +0100

    EjercicioT3

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -1,10 +1,29 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n;
+		boolean primo = true;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A continuación se le va a pedir un número y se le mostrará los números primos que hay entre ese número y el 1");
+		System.out.print("Introduzca un número, por favor: ");
+		n = sc.nextInt();
+		
+		for(int i=1; i!=n; i++) {
+			for (int k = 2; k < i; k++) {
+				// Si el número es divisible por i deja de ser primo
+				if (i % k == 0) {
+					primo = false;
+					System.out.println(i);
+					break;
+				}
+			}
+		}
 	}
 
 }

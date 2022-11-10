a55f50941f3771fc8093ea64b1c356cc19ce3313
jsevilla <jsevilla@106-19.info2>
2022-11-10 14:42:13 +0100

    EjerciciosT3

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -1,9 +1,31 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n, i, j;
+		String punto = "*";
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A continuación se le pedirá un número y se generará un triangulo con esas dimensiones");
+		System.out.print("Introduzca un número, por favor: ");
+		n = sc.nextInt();
+		
+		for(i=1;i<=n;i++)
+		{
+    		for(j=i;j<=n;j++)
+    		{
+        			System.out.print(" ");
+    		}
+    		for(j=1;j<=i;j++)
+    		{
+        			System.out.print("*");
+    		}
+    		System.out.println("");
+		}	
 
 	}
 

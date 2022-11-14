8569041729e092d3dc85492fcf94e548a80c5fa7
jsevilla <jsevilla@106-19.info2>
2022-11-14 11:14:34 +0100

    EjercicioT3

--- src/ejercicios/Ejercicio6.java
+++ src/ejercicios/Ejercicio6.java
@@ -10,17 +10,41 @@ public class Ejercicio6 {
 		//Creamos un scanner y lo nombramos "sc"
 		Scanner sc = new Scanner(System.in);
 		
+		//Presentamos el programa al usuario y su funcionamiento
 		System.out.println("A continuación, el programa le va a pedir un número de 1 al 20 y hará una media prirámide contando hasta el número introducido");
+		//Pedimos un número al usuario
 		System.out.print("Inserte un número del 1 al 20: ");
+		//Capturamos la respuesta y la guardamos en la variable "n"
 		n = sc.nextInt();
-		
+		//Comprobamos que el número esté dentro del rango establecido
+		if(n<=20&&n>0) {
+			
+			/*
+			 * En el bucle "for":
+			 * - Inicializamos la variable "i" a 1
+			 * - No se parará hasta llegar a "n"
+			 * - Suma 1 a la variable "i" cada vuelta
+			 */
 		for(int i=1;i<=n;i++) {
+			/*
+			 * En el bucle "for":
+			 * - Inicializamos la variable "j" a 1
+			 * - No se parará hasta llegar a "i"
+			 * - Suma 1 a la variable "j" cada vuelta
+			 * - Muestra la variable "i" "j" veces
+			 */
 			for(int j=1; j<=i; j++) {
 				System.out.print(i);
 			}
+			//Salto de línea después de cada número
 			System.out.println("");
 		}
-		
+		}
+		//Si no está dentro del rango muestra un mensaje de error
+		else {
+			System.out.println("ERROR:El valor introducido no está en el rango");
+		}
+		//Cerramos el scanner
 		sc.close();
 
 	}
--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -10,12 +10,18 @@ public class Ejercicio7 {
 		//Creamos un scanner y lo nombramos "sc"
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("A continuación, el programa le va a pedir un número de 1 al 20 y hará una media prirámide contando hasta el número introducido");
-		System.out.print("Inserte un número del 1 al 20: ");
+		//Presentamos el programa al usuario y su funcionamiento
+		System.out.println("A continuación, el programa le va a pedir un número y hará una media prirámide contando hasta el número introducido");
+		//Pedimos un número al usuario
+		System.out.print("Inserte un número: ");
+		//Capturamos la respuesta y la guardamos en la variable "n"
 		n = sc.nextInt();
 		
 		for(int i=1;i<=n;i++) {
 			for(int j=1; j<=i; j++) {
+				System.out.print(j);
+			}
+			for(int j = i-1; j>=1; j--) {
 				System.out.print(j);
 			}
 			System.out.println("");

a386fbf775e809d9a7231258f997e951000dfa98
jsevilla <jsevilla@106-19.info2>
2022-11-15 12:50:46 +0100

    EjerciciosT3

--- src/ejercicios/Ejercicio10.java
+++ src/ejercicios/Ejercicio10.java
@@ -17,21 +17,28 @@ public class Ejercicio10 {
 		//Capturamos la respuesta y la guardamos en la variable "n"
 		n1 = sc.nextInt();
 		
+		//Igualamos n1 a n2 para poder manipular el número sin modificarlo
 		n1 = n2;
 		
+		//No parará hasta que n2 sea 0
 		while(n2!=0) {
+			//Obtenemos la ultima cifra y le damos el valor a la variable "cifra"
 			cifra = n2%10;
+			//Igualamos "n2" a "n2"/10
 			n2 = n2/10;
+			//Igualamos la variable "iverso" a si misma por 10 más la cifra
 			inverso = inverso*10 + cifra;
 		}
 		
+		//Si "n1" e "inverso" son iguales, es capicúa
 		if(n1 == inverso) {
-			System.out.println("Su número es capicua");
+			System.out.println("Su número es capicúa");
 		}
+		//Si no, no lo es
 		else {
-			System.out.println("Su número no es capicua");
+			System.out.println("Su número no es capicúa");
 		}
-		
+		//cerramos el scanner
 		sc.close();
 				
 
